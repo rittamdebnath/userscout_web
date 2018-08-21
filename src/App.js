7a571ebf51c7a28a708b17b0ef0c6ce711572931
Thakur Ballary <thakurballary@gmail.com>
2018-08-21 22:50:15 +0530

    Added CreatePost

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CreatePost from './components/CreatePost';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CreatePost />
+      </div>
+    );
+  }
+}
+
+export default App;
